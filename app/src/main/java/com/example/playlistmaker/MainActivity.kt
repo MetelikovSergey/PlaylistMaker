bfb96f18cd13a0b05d96ee400b3893eb649c11ba
MetelikovSergey <metelikov@yandex.ru>
2023-05-29 21:35:34 +0300

    Домашнее задание

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        class MainActivity : AppCompatActivity() {
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+                val search = findViewById<Button>(R.id.btn_search)
+                val media = findViewById<Button>(R.id.btn_media)
+                val settings = findViewById<Button>(R.id.btn_settings)
+                val searchIntent = Intent(this, SearchActivity::class.java)
+                val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+                    override fun onClick(v: View?) {
+                        startActivity(searchIntent)
+                    }
+                }
+                search.setOnClickListener(searchClickListener)
+
+
+                media.setOnClickListener {
+                    val mediaIntent = Intent(this, MediaActivity::class.java)
+                    startActivity(mediaIntent)
+                }
+
+
+                settings.setOnClickListener {
+                    val settingIntent = Intent(this, SettingActivity::class.java)
+                    startActivity(settingIntent)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.widget.ImageView
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.R
+
+
+class SettingActivity:AppCompatActivity(){override fun onCreate(savedInstanceState: Bundle?) {super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_setting)
+
+    val arrowBack = findViewById<ImageView>(R.id.btn_back)
+    arrowBack.setOnClickListener {val mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
+    }
+}
+}
