1a45ae3d0d824734074968be4a6b24efbe0cd3e1
MetelikovSergey <metelikov@yandex.ru>
2023-06-21 21:41:48 +0300

    Добавлена реализация экрана настройки

--- app/src/main/java/com/example/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.widget.ImageView
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.R
 
@@ -14,5 +16,25 @@ class SettingActivity : AppCompatActivity() {
 
         val arrowBack = findViewById<ImageView>(R.id.btn_back)
         arrowBack.setOnClickListener { finish() }
+
+        findViewById<TextView>(R.id.tvShare).setOnClickListener{
+            Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.share_ink))
+                type = "text/plain"
+                startActivity(this)
+            }
+        }
+        findViewById<TextView>(R.id.tvSupport).setOnClickListener{
+            Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_e_mail))
+                putExtra(Intent.EXTRA_SUBJECT,getString(R.string.massage_subject))
+                putExtra(Intent.EXTRA_TEXT,getString(R.string.message_text))
+                startActivity(this)
+            }
+        }
+        findViewById<TextView>(R.id.tvTerms).setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.web_page_links))))
+        }
     }
 }
